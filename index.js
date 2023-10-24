c0228eee517ac82f3ef79ecd058faf86b98e25f0
carlitaRP <carlapro2002@gmail.com>
2023-10-23 23:30:38 -0600

    integration in index.js and corrections

--- index.js
+++ index.js
@@ -33,6 +33,26 @@ app.get('/work-experience', (req, res, next) => {
       res.sendfile("assets/interests.json");
       });
 
+      app.get('/1a_dev_hours', (req, res, next) => {
+        res.sendfile("assets/1a_dev_hours.json");
+        });
+      app.get('/1a_proxy_size', (req, res, next) => {
+        res.sendfile("assets/1a_proxy_size.json");
+        });
+      
+      app.get('/3a_test1', (req, res, next) => {
+        res.sendfile("assets/3a_test1.json");
+        });
+      app.get('/3a_test2', (req, res, next) => {
+        res.sendfile("assets/3a_test2.json");
+        });
+      app.get('/3a_test3', (req, res, next) => {
+        res.sendfile("assets/3a_test3.json");
+        });
+      app.get('/3a_test4', (req, res, next) => {
+        res.sendfile("assets/3a_test4.json");
+        });
+
 app.listen(port,  () => 
 	console.log('listening on port ' + port
 ));
